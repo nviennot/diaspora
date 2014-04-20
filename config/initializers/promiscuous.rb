d6d697126e7aaa3a82f4ceda66026d59e32dd184
Nicolas Viennot <nicolas@viennot.biz>
2014-04-20 01:02:44 -0400

    Use specific redis

--- config/initializers/promiscuous.rb
+++ config/initializers/promiscuous.rb
@@ -1,6 +1,7 @@
 Promiscuous.configure do |config|
   config.publisher_exchange   = 'diaspora'
-  config.logger = Logger.new(STDERR)
-  config.logger.level = 0
-  config.hash_size = 0
+  config.subscriber_exchanges = ['diaspora-topic']
+  config.logger = Logger.new(STDERR).tap { |l| l.level = 0 }
+  config.instrumentation_file = '/tmp/instrumentation.log'
+  config.redis_url            = 'redis://localhost:7776/'
 end
